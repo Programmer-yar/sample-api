bbf5dabb6560b43e8eee117e7a678f93b65b7c30
yar <ahmadyar228@gmail.com>
2022-10-08 15:30:07 +0500

    database update

--- project/settings.py
+++ project/settings.py
@@ -75,12 +75,23 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'task_logger',
+		'USER': 'yar',
+		'PASSWORD': 'mysql-testpass-123',
+		'HOST':'34.132.135.229',
+		'PORT':'3306',
+	}
 }
+
 
 
 # Password validation
